82c5c825f2e9eac274336f60ce8849476500db3c
Keiran Young <KcYoung1997@hotmail.co.uk>
2021-07-20 22:29:59 +0100

    ItemValue working
    Overloaded several contsructors due to them overwriting attributes from the main Item constructor.
    
    Iterate through slots of containers, and cartridges of mags.
    
    Multiplier for dogtag level.
    
    Multiplier for stack size

--- TarkovMultiLoader/Mods/ItemValue.cs
+++ TarkovMultiLoader/Mods/ItemValue.cs
@@ -10,35 +10,38 @@ class ItemValuePatch
 {
     static void Postfix(ref Item __instance, string id, ItemTemplate template)
     {
-
-        GClass1758 attr = new GClass1758(EItemAttributeId.MoneySum);
-        attr.Name = "RUB ₽";
-        attr.StringValue = new Func<string>(priceCalc(__instance, template));
-        attr.DisplayType = new Func<EItemAttributeDisplayType>(() => EItemAttributeDisplayType.Compact);
-        __instance.Attributes.Add(attr);
+        Mods.Value.AddItemValue(ref __instance, id, template);
     }
+}
 
-    static Func<string> priceCalc(Item __instance, ItemTemplate template) {
-        return () =>
-        {
-            float price = template.CreditsPrice;
-
-            var medKit = __instance.GetItemComponent<MedKitComponent>();
-            if (medKit != null)
-            {
-                price *= medKit.HpResource / medKit.MaxHpResource;
-            }
-
-            var repair = __instance.GetItemComponent<RepairableComponent>();
-            if (repair != null)
-            {
-                price *= repair.Durability / repair.MaxDurability;
-            }
-
-            return Math.Round(price).ToString();
-        };
+[HarmonyPatch(typeof(GClass1746), MethodType.Constructor)]
+[HarmonyPatch(new Type[] { typeof(string), typeof(AmmoTemplate) })]
+class AmmoValuePatch
+{
+    static void Postfix(ref GClass1746 __instance, string id, AmmoTemplate template)
+    {
+        Mods.Value.AddItemValue(ref __instance, id, template);
     }
+}
 
+[HarmonyPatch(typeof(GClass1748), MethodType.Constructor)]
+[HarmonyPatch(new Type[] { typeof(string), typeof(GClass1648) })]
+class GrenadeValuePatch
+{
+    static void Postfix(ref GClass1748 __instance, string id, GClass1648 template)
+    {
+        Mods.Value.AddItemValue(ref __instance, id, template);
+    }
+}
+
+[HarmonyPatch(typeof(GClass1712), MethodType.Constructor)]
+[HarmonyPatch(new Type[] { typeof(string), typeof(GClass1615) })]
+class ContainerValuePatch
+{
+    static void Postfix(ref GClass1748 __instance, string id, GClass1615 template)
+    {
+        Mods.Value.AddItemValue(ref __instance, id, template);
+    }
 }
 
 namespace Mods
@@ -50,6 +53,76 @@ namespace Mods
             base.Init(options);
             var harmony = new HarmonyLib.Harmony("com.kcy.ItemValuePatch");
             harmony.PatchAll();
+        }
+
+        public static void AddItemValue<T>(ref T __instance, string id, ItemTemplate template) where T: Item
+        {
+            // TODO: remove items of 0 cost
+            //__instance.Attributes.RemoveFirst(a => a.Name == "RUB ₽");
+            var atts = new List<GClass1758>();
+            atts.AddRange(__instance.Attributes);
+            __instance.Attributes = atts;
+
+            GClass1758 attr = new GClass1758(EItemAttributeId.MoneySum);
+            attr.Name = "RUB ₽";
+            attr.StringValue = new Func<string>(__instance.ValueStr);
+            attr.DisplayType = new Func<EItemAttributeDisplayType>(() => EItemAttributeDisplayType.Compact);
+            __instance.Attributes.Add(attr);
+        }
+    }
+
+    public static class ValueExtension
+    {
+        public static double Value(this Item item)
+        {
+            double price = item.Template.CreditsPrice;
+
+            // Container
+            if (item is GClass1662 container)
+            {
+                foreach (var slot in container.Slots)
+                {
+                    foreach (var i in slot.Items)
+                    {
+                        price += i.Value();
+                    }
+                }
+            }
+
+            if (item is GClass1700 mag)
+            {
+                foreach (var i in mag.Cartridges.Items)
+                {
+                    price += i.Value();
+                }
+                
+            }
+            
+            var medKit = item.GetItemComponent<MedKitComponent>();
+            if (medKit != null)
+            {
+                price *= medKit.HpResource / medKit.MaxHpResource;
+            }
+
+            var repair = item.GetItemComponent<RepairableComponent>();
+            if (repair != null)
+            {
+                price *= repair.Durability / repair.MaxDurability;
+            }
+
+            var dogtag = item.GetItemComponent<DogtagComponent>();
+            if (dogtag != null)
+            {
+                price *= dogtag.Level;
+            }
+
+            price *= item.StackObjectsCount;
+
+            return price;
+        }
+        public static string ValueStr(this Item item)
+        {
+            return Math.Round(item.Value()).ToString();
         }
     }
 }

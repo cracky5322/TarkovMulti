00f44d8f26bfd7c1c524b79deb5f8128c9b29caf
Keiran Young <KcYoung1997@hotmail.co.uk>
2021-07-21 19:58:43 +0100

    fix: remove hardcoded mods path for SPT

--- TarkovMultiLoader/SPTLoader.cs
+++ TarkovMultiLoader/SPTLoader.cs
@@ -1,19 +1,42 @@
-﻿using System;
+﻿
 using System.IO;
-using System.Linq;
-using System.Reflection;
 using UnityEngine;
 using Aki.Common;
+using System.Reflection;
 
 public class SPTLoader
 {
     private static void Main(string[] args)
     {
+        string folder = "";
+        // Search mods folder for module.dlls
+        foreach (var m in Directory.GetFiles(@"user\mods\", "module.dll", SearchOption.AllDirectories))
+        {
+            try {
+                
+                var name = AssemblyName.GetAssemblyName(m);
+                Log.Info(name.ToString());
+                // Check if the assembly name is our own
+                // TODO: this stops us from having seperate release builds in different folders for now.
+                if (name.Name == "KcY-TarkovMulti")
+                {
+                    // Get mod folder name
+                    folder = m.Split('\\')[2];
+                    break;
+                }
+            } catch { }
+        }
+        if (folder == "")
+        {
+            Log.Error("Unable to find own module.dll under 'user\\mods\\'. Stopping loader.");
+            return;
+        }
+
         Log.Info("Hello from modloader");
         var loader = new BaseLoader { mainObject = new GameObject("kcy-loader") };
         GameObject.DontDestroyOnLoad(loader.mainObject);
 
-        loader.modOptions = Json.Deserialize<ModOptions[]>(File.ReadAllText(@"user\mods\kcy-modloader\loader.json"));
+        loader.modOptions = Json.Deserialize<ModOptions[]>(File.ReadAllText(@"user\mods\"+folder+@"\loader.json"));
         Log.Info("Modloader config: " + Json.Serialize(loader.modOptions));
 
         loader.log = new SPTLog();
